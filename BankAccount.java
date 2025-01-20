826a0c26a323ca9fc43af4cd2ecaddcf3ade46fc
MahabirGupta <mahabirg81@gmail.com>
2025-01-20 22:04:56 +0800

    Made changes to BankAccount class to the method addTransaction

--- BankAccount.java
+++ BankAccount.java
@@ -3,42 +3,35 @@ import java.util.List;
 
 public class BankAccount {
     private final String accountId;
-    private double balance;
     private final List<Transaction> transactions;
 
     public BankAccount(String accountId) {
         this.accountId = accountId;
-        this.balance = 0.0;
         this.transactions = new ArrayList<>();
+    }
+
+    public boolean addTransaction(Transaction transaction) {
+        if (transaction.getType().equalsIgnoreCase("W")) {
+            double currentBalance = getBalance();
+            if (currentBalance < transaction.getAmount()) {
+                return false; // Insufficient balance
+            }
+        }
+        transactions.add(transaction);
+        return true;
     }
 
     public String getAccountId() {
         return accountId;
     }
 
-    public double getBalance() {
-        return balance;
-    }
-
     public List<Transaction> getTransactions() {
         return transactions;
     }
 
-    public boolean addTransaction(Transaction transaction) {
-        if (transaction.getType().equalsIgnoreCase("W") && balance < transaction.getAmount()) {
-            return false;
-        }
-        if (transaction.getType().equalsIgnoreCase("W")) {
-            balance -= transaction.getAmount();
-        } else if (transaction.getType().equalsIgnoreCase("D")) {
-            balance += transaction.getAmount();
-        }
-        transactions.add(transaction);
-        return true;
-    }
-
-    public void applyInterest(double interestAmount) {
-        balance += interestAmount;
-        transactions.add(new Transaction("I", interestAmount, null));
+    public double getBalance() {
+        return transactions.stream()
+                .mapToDouble(t -> t.getType().equalsIgnoreCase("D") ? t.getAmount() : -t.getAmount())
+                .sum();
     }
 }
