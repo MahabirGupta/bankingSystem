fbc5a754c4326f57a48e806af67a204cdfb03760
MahabirGupta <mahabirg81@gmail.com>
2025-01-20 14:31:48 +0800

    Create BankingSystem class to implement the methods for the different transactions and call the interestRules method

---
+++ BankingSystem.java
@@ -0,0 +1,42 @@
+import java.time.LocalDate;
+import java.util.*;
+
+public class BankingSystem {
+    private final Map<String, BankAccount> accounts;
+    private final List<InterestRule> interestRules;
+    private final Map<String, Integer> transactionCounter;
+
+    public BankingSystem() {
+        accounts = new HashMap<>();
+        interestRules = new ArrayList<>();
+        transactionCounter = new HashMap<>();
+    }
+
+    public boolean addTransaction(String accountId, String type, double amount) {
+        accounts.putIfAbsent(accountId, new BankAccount(accountId));
+        BankAccount account = accounts.get(accountId);
+
+        String txnId = generateTransactionId(accountId);
+        return account.addTransaction(new Transaction(type, amount, txnId));
+    }
+
+    private String generateTransactionId(String accountId) {
+        LocalDate date = LocalDate.now();
+        int counter = transactionCounter.getOrDefault(accountId, 0) + 1;
+        transactionCounter.put(accountId, counter);
+        return String.format("%s-%02d", date.toString().replaceAll("-", ""), counter);
+    }
+
+    public void addInterestRule(String ruleId, LocalDate date, double rate) {
+        interestRules.add(new InterestRule(date, ruleId, rate));
+    }
+
+    public BankAccount getAccount(String accountId) {
+        return accounts.get(accountId);
+    }
+
+    public List<InterestRule> getInterestRules() {
+        return interestRules;
+    }
+}
+
